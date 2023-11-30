479c43f35e0641518bbc48cc3600c4d0a5cdf257
Artur Matysiak <inz.matys@gmail.com>
2023-11-30 19:43:02 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+// for (let i = 0; i <= 5; i++) {
+//   result += i;
+//   console.log(result);
+// }
+
+let i = 0;
+while (i <= 5) {
+  result += i;
+  i++;
+  console.log(result);
+}
