2aaab5e14ec67fea4f6f080132844f434c78bed2
Artur Matysiak <inz.matys@gmail.com>
2023-11-30 19:32:12 +0100

    task done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,6 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log("Wylosowano: ", randomNumber);
+
+if (randomNumber > 5) {
+  for (let i = 5; i <= randomNumber; i++) console.log(i);
+} else console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
