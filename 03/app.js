298157cbd9c89aa6d51b90f17cbdd1b978c00514
Artur Matysiak <inz.matys@gmail.com>
2023-11-30 19:51:09 +0100

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x != randomNumber) {
+  randomNumber = (Math.random() * 10).toFixed();
+  console.log("wylosowanna to ", randomNumber);
+  iteration++;
+}
+console.log(`zgadłem za ${iteration} razem!`);
